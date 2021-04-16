7bf9eeac30bcc6e5737be18e82cc675d602ac7f4
Carlos Aznarán Laos <caznaranl@uni.pe>
2021-04-15 22:14:24 -0500

    First commit

---
+++ content/articles/hello.md
@@ -0,0 +1,12 @@
+Title: Hola mundo
+Date: 2021-04-15 08:00
+Modified: 2021-04-15 08:00
+Category: News
+Tags: pelican, python
+Slug: flex-pelican-theme
+
+[Flex theme](https://github.com/alexandrevicenzi/Flex) 2.4.0 comes with minor changes.
+
+- Updated translations (Italian, Spanish, Portuguese, German, Turkish)
+
+Hope you enjoy this version.
---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Aula B'
+SITENAME = 'Análisis y modelamiento numérico I 2021-1'
+# SITEURL = ''
+SITEURL = 'https://Analisis-Modelamiento-Numerico-I-2021-1.github.io'
+
+PATH = 'content'
+THEME = 'themes/Flex'
+TIMEZONE = 'America/Lima'
+
+DEFAULT_LANG = 'es'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Classroom', 'https://classroom.google.com/c/MzIxODk5MzQyMzQw'),
+         ('GitHub', 'https://github.com/Analisis-Modelamiento-Numerico-I-2021-1'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
